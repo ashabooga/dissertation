efea973fec3190c74e169efc8b3407d66eee4496
Ben Cuello-Wolffe <bcuellowolffe@gmail.com>
2024-02-15 18:59:58 +0000

    Finished draft for Practice View, made file for Break View

---
+++ ADHD Learning/Views/BreakView.swift
@@ -0,0 +1,18 @@
+//
+//  BreakView.swift
+//  ADHD Learning
+//
+//  Created by Ben Cuello-Wolffe on 2/15/24.
+//
+
+import SwiftUI
+
+struct BreakView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreakView()
+}
